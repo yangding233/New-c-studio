02f02d9d842b584ad8b415d14012262ddc1e812d
yangding233 <150337404+yangding233@users.noreply.github.com>
2024-09-19 00:50:57 +0800

    Add files via upload

--- firstclass.c
+++ firstclass.c
@@ -77,21 +77,80 @@
 //
 //	return 0;
 //}
+//
+//#include <stdio.h>
+//#include <string.h>
+//
+//
+//
+//int main()
+//{
+//	char arr[11] = "helloworld";      //注意：\0也占用一个字符,这里arr[11]实际只能访问到arr[10]=\0
+//
+//	int arr1[3] = { 1,2,3 };
+//	printf("%c\n", arr[10]);
+//	printf("%d\n", arr1[2]);              
+//
+//
+//
+//
+//	return 0;
+//}
+
+
+
+///////////////////////typedef重命名字符///////////////
+//#include <stdio.h>
+//#include <string.h>
+//
+//typedef unsigned int uint;//重命名字符
+//
+//
+//int main()
+//{
+//	unsigned int a = 0;
+//	uint b = 1;
+//	return 0;
+//}
+
+///////////////////static////////////////////
+//static可修饰局部变量 全局变量 函数  变为静态**
+
 
 #include <stdio.h>
 #include <string.h>
+#include "test01.h" //.h文件中就是使用了extern，只不过省略了
+//extern int gal;//既可以用头文件，也可以这么用
+//extern hello();
 
+static int c = 10;//static 修饰全局变量时外部连接属性变为内部链接属性，不能被extern
+static int c = 10;//static 修饰全局函数时外部连接属性变为内部链接属性，不能被extern，
+static test00(){}
+void test()
+{
+	static int a = 0;
+	//static修饰局部变量的时候，局部变量出了作用域，不销毁
+	// 本质上，static修饰局部变量时，改变了变量的储存位置
+	// 将栈区的局部变量变为静态区的静态变量 直到程序结束才销毁
+	a++;
+	printf("%d ", a);
+}
 
 
 int main()
 {
-	char arr[11] = "helloworld";      //注意：\0也占用一个字符,这里arr[11]实际只能访问到arr[10]=\0
-
-	int arr1[3] = { 1,2,3 };
-	printf("%c\n", arr[10]);
-	printf("%d\n", arr1[2]);              
 
 
+
+	for (int i = 0; i < 10; i++)
+	{
+		test();
+	}
+
+
+	printf("%d ", gal);
+
+	hello();
 
 
 	return 0;
---
+++ hello.c
@@ -0,0 +1,14 @@
+#define _CRT_SECURE_NO_WARNINGS
+//
+#include <stdio.h>
+#include <string.h>
+
+void hello()
+{
+	printf("hello world");
+}
+//int main()
+//{
+//	/*hello();*/
+//	return 0;
+//}
