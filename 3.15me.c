deadcb1d7db16d6f2d5eafeda6738281b5302f4e
yangding233 <150337404+yangding233@users.noreply.github.com>
2024-10-16 01:04:11 +0800

    Add files via upload

---
+++ 3.15me.c
@@ -0,0 +1,56 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+void move(int *a,int i)
+{
+    int p = a[i];
+    for (int j = i; j > 0;j--)
+    {
+        a[j] = a[j - 1];
+    }
+    a[0] = p;
+}
+
+
+
+
+bool find(int *a,int len,int into)
+{
+    for (int i = 0; i < len; i++)
+    {
+        if(into == a[i])
+        {
+            
+            move(a, i);
+            return 1;
+        }
+
+    }
+    return 0;
+}
+
+
+
+
+
+
+
+
+int main()
+{
+    int a[] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+    int len = sizeof(a) / sizeof(a[0]);
+    int into;
+    scanf("%d",&into);
+    find(a, len, into);
+
+    for (int i = 0; i < len;i++)
+    {
+        printf("%d ", a[i]);
+    }
+    return 0;
+}
